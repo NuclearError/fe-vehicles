cb440a4e14408ac6a0dc0d594e2654e92050a186
Angela Cullen <esmearmitage@gmail.com>
2024-11-28 14:15:10 +0000

    add unit tests for helper functions

--- src/api/__tests__/helpers.test.js
+++ src/api/__tests__/helpers.test.js
@@ -1,4 +1,4 @@
-import { request } from '../helpers';
+import { request, isVehicleDataValid, mergeVehicleData } from '../helpers';
 
 global.fetch = jest.fn();
 
@@ -44,6 +44,83 @@ describe('Helpers', () => {
 
       await expect(request('/api/error')).rejects.toThrow(networkErrorMessage);
       expect(fetch).toHaveBeenCalledWith('/api/error');
+    });
+  });
+
+  describe('isVehicleDataValid', () => {
+    it('should return true for vehicles with valid data', () => {
+      const vehicle = { id: 1, modelYear: 2020, media: {}, apiUrl: '/carA' };
+      expect(isVehicleDataValid(vehicle)).toBe(true);
+    })
+    test.each([
+      { id: 1, modelYear: 2020, media: {}, apiUrl: undefined },
+      { id: 2, modelYear: 2020, apiUrl: '/carA', media: undefined },
+      { id: 3, media: {}, apiUrl: '/carB', modelYear: undefined },
+      { modelyear: 2020, media: {}, apiUrl: '/carC', id: undefined },
+      {},
+    ])('should return false for vehicles with missing id, modelYear, media, or apiUrl', (vehicle) => {
+      expect(isVehicleDataValid(vehicle)).toBe(false);
+    });
+  });
+
+  describe('mergeVehicleData', () => {
+    const mockVehicle = { id: 1, modelYear: 2020, media: {} };
+    const mockDetails = {
+      description: 'Lorem ipsum',
+      price: "£65,100",
+      meta: {
+        passengers: 4,
+        drivetrain: ["A", "B"],
+        "bodystyles": ["lorem", "ipsum"],
+        emissions: {
+          template: '$value g/km',
+          value: 120,
+        },
+      },
+    };
+    it('should return null if details object has no price', () => {
+      const details = { description: 'Lorem ipsum', meta: {} };
+      const result = mergeVehicleData(mockVehicle, details);
+
+      expect(result).toBeNull();
+    });
+
+    it('should merge vehicle data with details, including processed emissions info', () => {
+      const { description, price } = mockDetails;
+      const { passengers, drivetrain, bodystyles } = mockDetails.meta;
+      const result = mergeVehicleData(mockVehicle, mockDetails);
+
+      expect(result).toEqual({
+        ...mockVehicle,
+        description,
+        price,
+        passengers,
+        drivetrain,
+        bodystyles,
+        emissions: "120 g/km"
+      });
+    });
+    it('should handle missing optional data gracefully', () => {
+      const mockDetailsWithMissingInfo = {
+        ...mockDetails,
+        meta: {
+          ...mockDetails.meta,
+          bodystyles: undefined,
+          emissions: undefined,
+        },
+      }
+      const { description, price } = mockDetails;
+      const result = mergeVehicleData(mockVehicle, mockDetailsWithMissingInfo);
+
+      expect(result).toEqual({
+        ...mockVehicle,
+        description,
+        price,
+        passengers: mockDetails.meta.passengers,
+        drivetrain: mockDetails.meta.drivetrain,
+        bodystyles: undefined,
+        emissions: undefined,
+      });
     });
   });
 });
