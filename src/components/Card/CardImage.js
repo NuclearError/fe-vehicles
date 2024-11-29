1eea656bcfa30fde56703afa60b41d4ce10e4081
Angela Cullen <esmearmitage@gmail.com>
2024-11-29 09:11:20 +0000

    ignore alt tag rule - there's a valid accessibility-based reason for this, see notes in file

--- src/components/Card/CardImage.js
+++ src/components/Card/CardImage.js
@@ -1,24 +1,27 @@
+/* eslint-disable jsx-a11y/alt-text */
+
 import React from 'react';
 import './style.scss';
 
 export const CardImage = ({ id, modelYear }) => {
-
   /*
     This element is aria-hidden (with no alt tag on the image)
-    because the image itself is not interactive and can be 
-    considered a 'decorative' element. Alt tags are best reserved 
-    for images which convey meaningful information that would otherwise
-    not be present in the rest of the DOM.
+    because the image itself is not interactive and can be
+    considered a 'decorative' element (within the limited context of this technical test!).
+    Alt tags are best reserved for images which convey meaningful information
+    that would otherwise not be present in the rest of the DOM.
   */
 
-  return <div data-testid={`cardImage-${id}`} className="VehicleList__CardImage" aria-hidden>
-    <picture>
-      {/*
-        In real circumstances we would likely use many source elements, 
+  return (
+    <div data-testid={`cardImage-${id}`} className="VehicleList__CardImage" aria-hidden>
+      <picture>
+        {/*
+        In real circumstances we would likely use many source elements,
         mapping over a range of images coming from a CDN
       */}
-      <source srcSet={`/images/16x9/${id}_${modelYear}.jpg`} media="(min-width: 768px)" />
-      <img src={`/images/1x1/${id}_${modelYear}.jpg`} />
-    </picture>
-  </div>
-}
+        <source srcSet={`/images/16x9/${id}_${modelYear}.jpg`} media="(min-width: 768px)" />
+        <img src={`/images/1x1/${id}_${modelYear}.jpg`} />
+      </picture>
+    </div>
+  );
+};
