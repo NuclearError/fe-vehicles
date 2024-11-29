b12512e72c7a83c41616d77f1d2e4dcde7e4bddc
Angela Cullen <esmearmitage@gmail.com>
2024-11-29 08:49:11 +0000

    Fixed screen reader accessibility issue with price text

--- src/components/Card/Price.js
+++ src/components/Card/Price.js
@@ -1,4 +1,13 @@
 import React from 'react';
 import './style.scss';
 
-export const Price = ({ price }) => <h3 className='VehicleList__CardTextPrice'>From {price}</h3>;
+/* 
+  NB. Text content *must* be a template literal string in order to 
+  avoid being rendered as separate nodes in the DOM.
+  Visually it doesn't matter but screen readers will read separate 
+  nodes as if they are separate 'sentences'. 
+  The template literal string means the DOM will always interpret
+  the text as a single node.
+*/
+
+export const Price = ({ price }) => <h3 className='VehicleList__CardTextPrice'>{`From ${price}`}</h3>;
