fea08bbf4a4257281caa6400f3c3cfb46aa23690
Angela Cullen <esmearmitage@gmail.com>
2024-11-29 08:28:00 +0000

    style details/summary content

--- src/components/Card/CardDetails.js
+++ src/components/Card/CardDetails.js
@@ -13,10 +13,10 @@ export const CardDetails = ({ description, bodystyles, emissions }) => {
 
   return (
     <details className="VehicleList__CardDetails" onToggle={handleToggle}>
-      <summary>{isOpen ? 'Read less' : 'Read more'}</summary>
+      <summary>{isOpen ? 'Show less' : 'Show more'}</summary>
       <div className='VehicleList__CardDetailsContent'>
         <p className='VehicleList__CardDetailsPara'>{description}</p>
-        <p className='VehicleList__CardDetailsPara--highlight'>{emissions}</p>
+        <p className='VehicleList__CardDetailsPara VehicleList__CardDetailsPara--highlight'>{emissions}</p>
         <ul className='VehicleList__CardDetailsList'>
           {bodystyles.map(style =>
             <li className='VehicleList__CardDetailsListItem' key={`bodystyle-${style}`}>{style}</li>
